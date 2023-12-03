97af49aeb8af6f712a51895e16370c9c35730828
han18 <han.fadah@gmail.com>
2023-12-03 16:55:52 -0500

    implemeted the logic of the score if it goes lower the zero

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ document.querySelector(".score").textContent = "20";
 const secretNumber = Math.trunc(Math.random() * 10) + 1;
 document.querySelector(".number").textContent = secretNumber;
 
+// creating a variable decrease the number of times a user plays
+let scoreCheck = 5;
+
 // selecting a button because a button triggers an event. the click event requires a second argument called eventHandler  to do something with the event and we pass in a function with its value
 document.querySelector(".check").addEventListener("click", function () {
   const guessNum = Number(document.querySelector(".guess").value);
@@ -18,8 +21,22 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guessNum === secretNumber) {
     document.querySelector(".message").textContent = "You WON!";
   } else if (guessNum > secretNumber) {
-    document.querySelector(".message").textContent = "Too High";
+    if (scoreCheck > 0) {
+      document.querySelector(".message").textContent = "Too High";
+      scoreCheck--;
+      document.querySelector(".score").textContent = scoreCheck;
+    } else {
+      document.querySelector(".message").textContent = "YOU LOST!";
+    }
   } else if (guessNum < secretNumber) {
-    document.querySelector(".message").textContent = "Too Low!";
+    if (scoreCheck > 0) {
+      document.querySelector(".message").textContent = "Too Low!";
+      scoreCheck--;
+      document.querySelector(".score").textContent = scoreCheck;
+    } else {
+      document.querySelector(".message").textContent = "YOU LOST!";
+    }
   }
 });
+
+// my own code
