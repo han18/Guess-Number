4f6badeb618a458fa71511767df2f0c4a311a616
han18 <han.fadah@gmail.com>
2023-12-03 17:10:17 -0500

    created a function to hold the high low score logic from reapeting in the if statment

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 "use strict";
 
-// console.log((document.querySelector(".message").textContent = "Start Gue😎😎😎"));
-
-// document.querySelector(".message").textContent = "Correct Number! 😎";
-
 document.querySelector(".number").textContent = "❖";
 document.querySelector(".score").textContent = "20";
 
@@ -13,6 +9,16 @@ document.querySelector(".number").textContent = secretNumber;
 // creating a variable decrease the number of times a user plays
 let scoreCheck = 5;
 
+// see if I could implement an function to do the repetitive code
+function scoreRepeat() {
+  if (scoreCheck > 0) {
+    document.querySelector(".message").textContent = "Too High";
+    scoreCheck--;
+    document.querySelector(".score").textContent = scoreCheck;
+  } else {
+    document.querySelector(".message").textContent = "YOU LOST!";
+  }
+}
 // selecting a button because a button triggers an event. the click event requires a second argument called eventHandler  to do something with the event and we pass in a function with its value
 document.querySelector(".check").addEventListener("click", function () {
   const guessNum = Number(document.querySelector(".guess").value);
@@ -21,13 +27,7 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guessNum === secretNumber) {
     document.querySelector(".message").textContent = "You WON!";
   } else if (guessNum > secretNumber) {
-    if (scoreCheck > 0) {
-      document.querySelector(".message").textContent = "Too High";
-      scoreCheck--;
-      document.querySelector(".score").textContent = scoreCheck;
-    } else {
-      document.querySelector(".message").textContent = "YOU LOST!";
-    }
+    scoreRepeat();
   } else if (guessNum < secretNumber) {
     if (scoreCheck > 0) {
       document.querySelector(".message").textContent = "Too Low!";
